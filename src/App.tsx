331e82328fd8ed6ed828db47d6432f1359cfb89b
ou guangye <3250679635@qq.com>
2023-03-14 17:39:27 +0800

    重构为ts项目
    屏蔽.idea文件夹

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
 import React, { useRef } from "react";
-import { useState } from "react";
-import { useEffect } from "react"
-import domtoimage from 'dom-to-image';
+import { useState, useEffect, ChangeEvent } from "react";
+import domToImage from 'dom-to-image';
 import { saveAs } from 'file-saver';
 import JsZip from 'jszip';
+
+/**
+ * 导入UI 组件库
+ */
 
 import { DownloadOutlined, UploadOutlined} from '@ant-design/icons';
 import { Button, Pagination, Space, Radio} from 'antd';
 import 'antd/dist/antd.css';
 
+/**
+ * 导入资源
+ */
 import './assets/css/index.css'
 
-const border_image_list = [require('../src/assets/photo/black.png'), require('../src/assets/photo/white.png')]
+let border_image_list : string[] = [require('../src/assets/photo/black.png'), require('../src/assets/photo/white.png')]
 
 const App = () => {
     const [border, setBorder] = useState(0); // 0 表示 黑色边框, 1 表示 白色边框
-    const [image_list, setImageList] = useState([])
-    const [name_list, setNameList] = useState([])
+    const [image_list, setImageList] = useState([] as string[])
+    const [name_list, setNameList] = useState([] as string[])
     const [image_index, setImageIndex] = useState(0) // 设置图片显示index
-    const [blobs, setBlobs] = useState([])
+    const [blobs, setBlobs] = useState([] as any[])
     const [loading, setLoading] = useState(false); // 避免downLoading 按钮被点击太多次
 
-    const inputRef = useRef(null)
+    const inputRef = useRef<HTMLInputElement>(null)
     const phoneRef = useRef(null)
 
     useEffect(()=>{
@@ -35,34 +41,40 @@ const App = () => {
         cutPhoto()
     }, [image_index])
 
-    const readPhoto = e => {
+    const readPhoto = (e : ChangeEvent<HTMLInputElement>) => {
         try {
-            const files = e.target.files
+            const files = e.target?.files
+            if(!files) {
+                throw new Error("Upload FileList is null")
+            }
             if(files.length !== 0) setImageList([])
             for (let i = 0; i < files.length; i++ ) {
                 // 提取文件名字
                 const reg = /(.*)\.(.*)/
                 const file = files[i]
-                setNameList(current => [...current, file.name.match(reg)[1].trim()])
+                if(!file.name.match(reg)) {
+                    throw new Error("match name list is null")
+                }
+                setNameList(current => [...current, ((file.name.match(reg) as string[])[1]).trim()])
                 //读取文件内容
                 const reader = new FileReader();
                 reader.readAsDataURL(file);
                 reader.onload = e => {
-                    setImageList(current => [...current, e.target.result])
+                    setImageList(current => [...current, e.target?.result as string])
                 };
             }
           } catch (error) {
             console.log(error)
-          } 
+          }
     }
 
     const uploadFiles = () => {
-        inputRef.current.click()
+        inputRef.current?.click()
     }
 
     const getSingleBlobPng = () => {
         const node = phoneRef.current
-        domtoimage.toBlob(node).then((blob) => {
+        domToImage.toBlob(node).then((blob) => {
             saveAs(blob, `${name_list[image_index]}.png`)
         })
     }
@@ -75,12 +87,12 @@ const App = () => {
     }
 
     const cutPhoto = () => {
-        domtoimage.toBlob(phoneRef.current).then((blob) => {
+        domToImage.toBlob(phoneRef.current).then((blob) => {
             console.log('entering')
-            setBlobs(current=>[...current, blob])  
+            setBlobs(current=>[...current, blob])
             setImageIndex(current=> current + 1)
         })
-    } 
+    }
 
     // 最终导出zip
     const exportZip = (res) => {
@@ -112,10 +124,10 @@ const App = () => {
                         <Radio.Button value={1}>white</Radio.Button>
                     </Radio.Group>
                 </div>
-                
+
 
                 <Button type="primary" icon={<UploadOutlined/>} size="large" onClick={uploadFiles} style={{marginTop:40 + 'px'}}>
-                    <input id="upload-photo" type='file' multiple="multiple" onChange={readPhoto} ref={inputRef}/>
+                    <input id="upload-photo" type='file' multiple onChange={readPhoto} ref={inputRef}/>
                     <span>Upload Picture</span>
                 </Button>
 
@@ -127,13 +139,13 @@ const App = () => {
                         Download All
                     </Button>
                 </Space>
-                
+
                 {
                     image_list.length > 1 ? (
                         <div style={{marginTop: 40 + 'px'}}>
-                            <Pagination 
-                                current={image_index + 1} 
-                                total={image_list.length} 
+                            <Pagination
+                                current={image_index + 1}
+                                total={image_list.length}
                                 defaultPageSize={1}
                                 onChange={page=>{setImageIndex(page-1)}}
                             />
